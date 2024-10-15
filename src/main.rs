a4cf9c96274fb06c05c6ee064daafd8dc9ef7174
sreejithliterally <imsreejithofficial@gmail.com>
2024-10-16 02:20:03 +0530

    something

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("your secret number is {secret_number}");
     println!("Please input your guess.");
     let mut guess = String::new();
 
